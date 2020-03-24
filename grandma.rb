16d9fdbbe1bf74183457394cbaa8d8f0bfd48f15
Ryan Parrish <rino3717@gmail.com>
2020-03-24 03:24:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRADNMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-    
+  elseif 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
