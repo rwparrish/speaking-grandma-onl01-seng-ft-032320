74ab5dd912f6afc02330ae16376bcc06fb03b2ad
Ryan Parrish <rino3717@gmail.com>
2020-03-24 03:35:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
 phrase = gets.chomp
 
+if phrase =
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
