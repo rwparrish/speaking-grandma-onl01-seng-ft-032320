8a7247097cae7fad0c5c4eb5473826e14ee54027
Ryan Parrish <rino3717@gmail.com>
2020-03-24 03:32:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma (phrase="I LOVE YOU GRANDMA!")
-  if phrase == "I LOVE YOU GRADNMA!"
+  if phrase == #{"phrase"}
     
     puts "I LOVE YOU TOO PUMPKIN!"
     
